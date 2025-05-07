d1628b197795ed944e253642a9ebdd1ff109dfdd
Anna Tiberg <anastasia-elisabeth.tiberg@elev.ga.ntig.se>
2025-05-07 11:22:58 +0200

    Klar med client

--- src/MulticastChatClient.java
+++ src/MulticastChatClient.java
@@ -10,7 +10,7 @@ public class MulticastChatClient {
             throws Exception {
 
         //Default port number we are going to use
-        int portnumber = 5000;
+        int portnumber = 5010;
         if (args.length >= 1) {
             portnumber = Integer.parseInt(args[0]);
         }
